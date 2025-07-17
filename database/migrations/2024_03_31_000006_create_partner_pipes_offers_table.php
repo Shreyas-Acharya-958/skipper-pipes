1ee822cba65c3e8286d3b58b02f1a02d04558615
palkeshpatel <patel.palkesh@gmail.com>
2025-07-17 17:22:59 +0530

    update migation

--- database/migrations/2024_03_31_000006_create_partner_pipes_offers_table.php
+++ database/migrations/2024_03_31_000006_create_partner_pipes_offers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('partner_pipes_offers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('partner_id')->constrained()->onDelete('cascade');
-            $table->string('image');
+            $table->longText('image');
             $table->timestamps();
         });
     }
