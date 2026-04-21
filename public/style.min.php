66661535d5dc5ac3cd870a86cea07eca21222748
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-04-21 13:19:04 +0530

    Speed Fix

---
+++ public/style.min.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: text/css");
+header("Cache-Control: public, max-age=86400");
+
+$cssFiles = [
+    'style.css'
+];
+
+$combined = '';
+foreach ($cssFiles as $file) {
+    if (file_exists(__DIR__ . '/' . $file)) {
+        $combined .= file_get_contents(__DIR__ . '/' . $file);
+    }
+}
+
+// Basic minification
+$combined = preg_replace('!/\*.*?\*/!s', '', $combined);
+$combined = preg_replace('/\s+/', ' ', $combined);
+
+echo $combined;
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -62,8 +62,7 @@
 
     <!-- ========== Start Stylesheet ========== -->
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-    {{-- 
-    <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet" /> --}}
+    {{--  <link href="{{ asset('assets/css/font-awesome.min.css') }}" rel="stylesheet" /> --}}
     <link rel="dns-prefetch" href="//cdnjs.cloudflare.com">
     <link rel="dns-prefetch" href="//unpkg.com">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet" />
@@ -77,8 +76,7 @@
     {{-- <link href="{{ asset('assets/css/bootsnav.css') }}" rel="stylesheet" /> --}}
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.php') }}">   
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
-    
+    <link rel="stylesheet" href="{{ asset('style.min.php') }}">   
     <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet" />
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-V8XGVP5J7B"></script>
     <script>function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-V8XGVP5J7B");</script>
