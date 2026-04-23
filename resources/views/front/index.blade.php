fe2a896692f82b206d18cf52fbd2cc8948fcfa12
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-04-23 12:49:00 +0530

    Page Speed

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -79,7 +79,10 @@
             <div class="carousel-inner">
                 @foreach ($banners as $key => $banner)
                     <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" data-interval="3000">
-                        <img src="{{ asset('storage/' . $banner->image) }}" class="d-block w-100" alt="{{ image_alt_text('storage/' . $banner->image, $banner->title) }}" {{ $key != 0 ? 'loading=lazy' : 'fetchpriority="high"' }}>
+                        <img src="{{ asset('storage/' . $banner->image) }}" class="d-block w-100" alt="{{ image_alt_text('storage/' . $banner->image, $banner->title) }}" {{ $key == 0 
+      ? 'fetchpriority="high" loading="eager"' 
+      : 'loading="lazy"' 
+  }} >
                     </div>
                 @endforeach
             </div>
@@ -107,7 +110,10 @@
                 @foreach ($banners as $key => $banner)
                     <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" data-interval="3000">
                         <img src="{{ asset('storage/' . $banner->mobile_image) }}" class="d-block w-100"
-                            alt="{{ image_alt_text('storage/' . $banner->mobile_image, $banner->title) }}" {{ $key != 0 ? 'loading=lazy' : 'fetchpriority="high"' }}
+                            alt="{{ image_alt_text('storage/' . $banner->mobile_image, $banner->title) }}" {{ $key == 0 
+      ? 'fetchpriority="high" loading="eager"' 
+      : 'loading="lazy"' 
+  }} 
                     </div>
                 @endforeach
             </div>
