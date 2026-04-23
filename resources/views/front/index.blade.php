a5cabbe965017376b5413801789e4f432fd95c4b
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-04-23 12:34:54 +0530

    Page Speed

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -78,7 +78,7 @@
         <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
             <div class="carousel-inner">
                 @foreach ($banners as $key => $banner)
-                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" data-interval="3000">
+                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" {{ $key != 0 ? 'loading=lazy' : '' }} data-interval="3000">
                         <img src="{{ asset('storage/' . $banner->image) }}" class="d-block w-100" alt="{{ image_alt_text('storage/' . $banner->image, $banner->title) }}" fetchpriority="high">
                     </div>
                 @endforeach
