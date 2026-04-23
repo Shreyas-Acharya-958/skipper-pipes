3617c9cc64f056b52c93b1bb969f6fa5cd0295ed
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-04-23 12:53:07 +0530

    Page Speed

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -107,7 +107,7 @@
                 @foreach ($banners as $key => $banner)
                     <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" data-interval="3000">
                         <img src="{{ asset('storage/' . $banner->mobile_image) }}" class="d-block w-100"
-                            alt="{{ image_alt_text('storage/' . $banner->mobile_image, $banner->title) }}" 
+                            alt="{{ image_alt_text('storage/' . $banner->mobile_image, $banner->title) }}" {{ $key != 0 ? 'loading=lazy' : 'fetchpriority="high"' }} >
                     </div>
                 @endforeach
             </div>
