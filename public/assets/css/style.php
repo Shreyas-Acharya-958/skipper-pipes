f391c9790e8518b476a82b993c8e4bcff58ccd7b
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-05-06 14:25:40 +0530

    fix overview page

--- public/assets/css/style.php
+++ public/assets/css/style.php
@@ -2,27 +2,29 @@
 header("Content-Type: text/css");
 header("Cache-Control: public, max-age=2592000");
 
+$basePath = __DIR__; // /css/
+
 $cssFiles = [
     'font-awesome.min.css',
-    // 'animate.css',
     'owl.carousel.min.css',
     'owl.theme.default.min.css',
-    'swiper.min.css',
+    '../js/swiper/swiper.min.css', // from /css/ to /js/
     'magnific-popup.css',
     'bootsnav.css',
-    // 'responsive.css',
     'flaticon-set.css',
     'themify-icons.css',
     'sweetalert2.min.css'
 ];
 
 $combined = '';
+
 foreach ($cssFiles as $file) {
-    if (file_exists(__DIR__ . '/' . $file)) {
-        $combined .= file_get_contents(__DIR__ . '/' . $file);
+    $fullPath = realpath($basePath . '/' . $file);
+
+    if ($fullPath && file_exists($fullPath)) {
+        $combined .= file_get_contents($fullPath) . "\n";
     }
 }
-
 // Basic minification
 $combined = preg_replace('!/\*.*?\*/!s', '', $combined);
 $combined = preg_replace('/\s+/', ' ', $combined);
