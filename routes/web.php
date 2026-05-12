dc365744a26c904b8e25ff599dce59f60b0ecbba
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-05-12 15:12:35 +0530

    fixed redidrect

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::permanentRedirect('/top-benefits-and-applications-of-swr-pipes-and-fittin
 Route::permanentRedirect('/products/upvc-pipes', '/products/upvc-pipes-and-fittings');
 Route::permanentRedirect('/products/cpvc-pipes', '/products/cpvc-pipes-and-fittings');
 Route::permanentRedirect('/products/swr-pipes', '/products/swr-pipes-and-fittings');
-Route::permanentRedirect('/products/marina-tank', '/products/water-storage-tank');
+Route::permanentRedirect('/products/marina-tank', '/products/marina-tank');
 Route::permanentRedirect('/private-project', '/private-projects');
 Route::permanentRedirect('/blogs/flagbearers-of-safe-water-skipper-pipes-leading-the-way', '/blogs/skipper-safe-water-distribution-piping-solutions');
 Route::permanentRedirect('/blogs/the-future-of-plumbing-why-cpvc-pipes-are-becoming-the-industry-standard', '/blogs/cpvc-pipes-industry-standard-plumbing');
