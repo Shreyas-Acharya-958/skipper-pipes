661642c9aa677b76d0027eab9c60c921ff7f11de
Shreyas-Acharya-09 <shreyasacharya966@gmail.com>
2026-05-25 13:01:48 +0530

    sitemap & homepage video bg img updated

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -356,7 +356,7 @@
     @if ($sectionThree && $sectionThree->title)
         <section class="trusted-one">
             <div class="trusted-one__bg jarallax" data-jarallax data-speed="0.2" data-imgPosition="50% 0%"
-                style='background-image: url("{{asset('assets/img/skipper-pipes-videos.webp')}}");'>
+                style='background-image: url("{{asset('assets/img/Skipper-pipes-homepage-video-banner.webp')}}");'>
             </div>
             <div class="container">
                 <div class="trusted-one__inner" data-aos="fade-up" data-aos-duration="1000">
