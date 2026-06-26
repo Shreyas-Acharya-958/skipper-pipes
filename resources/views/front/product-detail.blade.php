d42f95124fa6360c805031f04083f2a50afedf07
Kaushik Thakkar <kaushik.moment@outlook.com>
2026-06-26 16:14:54 +0530

    Button Label

--- resources/views/front/product-detail.blade.php
+++ resources/views/front/product-detail.blade.php
@@ -221,9 +221,15 @@
                                 href="{{ asset('storage/' . $product->brochure) }}"
                                 data-file-name="{{ $fileName }}"
                                 data-file-extension="{{ $fileExtension }}"
-                                data-text="Download Product Brochure"
+                                data-text="{{ 
+                                 empty(!$product->technical_brochure) ? "Download Product Brochure" : "Download Brochure";  
+                                }}"
                                 download>
+                                @if($product->technical_brochure)
                                 Download Product Brochure
+                                @else 
+                                Download Brochure
+                                @endif
                             </a>
                             @if($product->technical_brochure)
                             <a class="btn btn-light effect ml-5 btn-md js-download-brochure"
@@ -331,9 +337,15 @@
         href="{{ asset('storage/' . $product->brochure) }}"
         data-file-name="{{ $product->brochure }}"
         data-file-extension="{{ pathinfo($product->brochure, PATHINFO_EXTENSION) }}"
-        data-text="Download Product Brochure"
+        data-text="{{ 
+            empty(!$product->technical_brochure) ? "Download Product Brochure" : "Download Brochure";  
+        }}"
         download>
+        @if($product->technical_brochure)
         Download Product Brochure
+        @else 
+        Download Brochure
+        @endif
     </a>
     @if($product->technical_brochure)
     {{-- <a class="btn btn-light effect btn-md js-download-brochure"
